3216ab3f6d52c26d395c00892ccbf0cf90a284e4
vjaykr <141932646+vjaykr@users.noreply.github.com>
2025-02-10 14:03:33 +0530

    update2nd

--- PITPROJECT/frontend/src/App.jsx
+++ PITPROJECT/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import AddArticle from './AddArticle'
+import ArticleList from './ArticleList'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,9 @@ function App() {
     <>
       <h1 className='text-danger'>hello from me </h1>
       <AddArticle />
-      
+      <ArticleList />
+
+
     </>
   )
 }
---
+++ PITPROJECT/frontend/src/ArticleList.jsx
@@ -0,0 +1,19 @@
+function ArticleList() {
+  return (
+    <div className="container">
+        <div className="row">
+            <div className="col-md-3">
+                <h1>Article 1</h1>
+                <p>
+                    lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                </p>
+           
+            
+            <button className="btn btn-danger">View details</button>
+             </div>
+        </div>
+    </div>
+  )
+}
+
+export default ArticleList;
