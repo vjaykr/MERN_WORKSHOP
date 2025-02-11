173777c02b296f934144157ccf2ea55aa9f8ea5d
vjaykr <141932646+vjaykr@users.noreply.github.com>
2025-02-11 14:50:01 +0530

    Update app.js

--- PITPROJECT/backend/app.js
+++ PITPROJECT/backend/app.js
@@ -1,40 +1,43 @@
-
-const express = require('express');
+let express = require('express');
 let cors = require('cors');
 let mongoose = require('mongoose');
 
-
 let app = express();
 app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-async function Connection(){
- await mongoose.connect("mongodb://localhost:27017/pitproject")
-console.log("Connected to Database");
-}
-
-
-app.get('/', function(res,req) {
-    let data = [
-        {
-            title: "Hello World",
-            body: "This is a sample blog post"
-        },
-        {
-            title: "Hello World 2",
-            body: "This is a sample blog post 2"
-        }
-       
-    ];
-    res.json(data);
+mongoose.connect('mongodb://localhost:27017/yourDatabaseName', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
 });
 
-app.post("/addarticle", function(req, res) {
-    console.log(req.body);
+let articleSchema = mongoose.Schema({
+    name: String,
+    body: String
+});
+
+let Article = mongoose.model("article", articleSchema);
+
+app.get("/", async function(req, res) {
+    let article = await Article.find();
+    console.log(article);
+    res.json(article);
+});
+
+
+
+app.post('/addarticle', async function(req, res) {
+    let article = new Article(req.body);
+    await article.save();
     res.json({ message: "Article added" });
 });
 
 app.listen(3001, function() {
-    console.log("Server is running on port 3001, visit http://localhost:3001");
+    console.log("Server started");
 });
