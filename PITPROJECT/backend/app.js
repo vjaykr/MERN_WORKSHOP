1134bd0c0e4b568340358bfc7b7953fe7ef6e3b9
vjaykr <141932646+vjaykr@users.noreply.github.com>
2025-02-11 14:20:23 +0530

    after

--- PITPROJECT/backend/app.js
+++ PITPROJECT/backend/app.js
@@ -1,8 +1,21 @@
-const express = require('express'); // Add this line
+
+const express = require('express');
+let cors = require('cors');
+let mongoose = require('mongoose');
+
 
 let app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(_, res) {
+async function Connection(){
+ await mongoose.connect("mongodb://localhost:27017/pitproject")
+console.log("Connected to Database");
+}
+
+
+app.get('/', function(res,req) {
     let data = [
         {
             title: "Hello World",
@@ -11,13 +24,15 @@ app.get('/', function(_, res) {
         {
             title: "Hello World 2",
             body: "This is a sample blog post 2"
-        },
-        {
-            title: "Hello World 3",
-            body: "This is a sample blog post 3"
         }
+       
     ];
     res.json(data);
+});
+
+app.post("/addarticle", function(req, res) {
+    console.log(req.body);
+    res.json({ message: "Article added" });
 });
 
 app.listen(3001, function() {
--- PITPROJECT/frontend/src/AddArticle.jsx
+++ PITPROJECT/frontend/src/AddArticle.jsx
@@ -1,31 +1,46 @@
-import { useState } from "react"
+import { useState } from "react";
+import axios from "axios";
 
-function AddArticle(){
-
-    let [name,setName] = useState("vijay")
-    let [body,setBody] = useState()
-
-
-    function addArticle(){
-        alert("hello")
+function AddArticle() {
+    let [name, setName] = useState("hi there");
+    let [body, setBody] = useState("i am vijay");
+    async function addArticle() 
+    {
+        let respoce = await axios.post("http://localhost:3001/addarticle", {
+            name,
+            body,
+        });
+    
     }
+
     return (
-        <form className='col-md-4 ms-auto me-auto'> 
-        <h1 className="text-center">add a new article</h1>
-            <div  className="mb-3">
-                <input className="form-control" type="text" 
-                placeholder="Article Name"/>
-                k
-                
+        <form className="col-md-4 ms-auto me-auto">
+            <h1 className="text-center">Add a new article</h1>
+            <div className="mb-3">
+                <input
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    type="text"
+                    placeholder="Article Name"
+                />
             </div>
 
             <div className="mb-3">
-                
-                <textarea className='form-control' placeholder="description"></textarea>
+                <textarea
+                    onChange={(e) => setBody(e.target.value)}
+                    className="form-control"
+                    placeholder="Description"
+                ></textarea>
             </div>
-            <button onClick={addArticle} type = "button" className='btn btn-warning text-white w-100'>Add </button>
+            <button
+                onClick={addArticle}
+                type="button"
+                className="btn btn-warning text-white w-100"
+            >
+                Add
+            </button>
         </form>
-    )
+    );
 }
 
 export default AddArticle;
